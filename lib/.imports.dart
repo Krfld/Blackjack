bc034d5d1dc195cc5d6dfe396c25cc914781d253
Krfld <rodrigofldias@gmail.com>
2021-10-12 22:23:51 +0100

    .

---
+++ lib/.imports.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+
+export './main.dart';
+export './screens/home.dart';
---
+++ lib/screens/home.dart
@@ -0,0 +1,14 @@
+import '../.imports.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Blackjack'),
+      ),
+    );
+  }
+}
