b80c4fd91047bd57a49a9511ea84676b682451e6
Krfld <rodrigofldias@gmail.com>
2021-10-18 14:50:38 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 
 import './.imports.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
